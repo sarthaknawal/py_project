db8a75dab905e2cb11df70f5400289abe6d5006c
sarthaknawal <63709711+sarthaknawal@users.noreply.github.com>
2020-04-25 13:47:57 +0530

    Add files via upload

---
+++ 1project/function/replace.py
@@ -0,0 +1,5 @@
+def rep(a):
+ x = a.replace("\n", "")
+ print(x)
+ f1 = open("output1.txt", "w")
+ f1.write(x)
---
+++ 1project/read.py
@@ -0,0 +1,6 @@
+from function.replace import *
+
+f = open("textfile.txt", "rt")
+a=(f.read())
+c = (rep(a))
+#f.close()
